67e1b5ca804431ed46d23c5897658947a5da9fe9
b06703026 <b06703026@ntu.edu.tw>
2020-01-03 18:18:40 +0800

    可以秀5個
    可以秀5個

--- 介面.py
+++ 介面.py
@@ -13,7 +13,7 @@ image.pack()
 # 上半部
 top_header = Label(window, text='請選擇權重：',
                    font='微軟正黑體', bg='white')
-top_header.place(x=105, y=73)
+top_header.place(x=205, y=73)
 
 
 # 權重欄位
@@ -26,29 +26,31 @@ class CreateSpinbox:
         self.spinbox.place(x=x2, y=73)
 
 
-taste = CreateSpinbox('美味', 250, 300)
-cp = CreateSpinbox('CP值', 400, 450)
-space = CreateSpinbox('環境', 550, 600)
-service = CreateSpinbox('服務', 700, 750)
+taste = CreateSpinbox('美味', 350, 400)
+cp = CreateSpinbox('CP值', 500, 550)
+space = CreateSpinbox('環境', 650, 700)
+service = CreateSpinbox('服務', 800, 850)
 # 下半部
 bottom_header = Label(window, text='以下為推薦您的餐廳：',
                       bg='white', font='微軟正黑體')
-bottom_header.place(x=140, y=203)
+bottom_header.place(x=240, y=203)
 
 
 # 結果欄位
 class CreateVar:
-    def __init__(self, y):
+    def __init__(self, x, y):
         self.var = StringVar()
         self.label = Label(window, textvariable=self.var,
                            bg='white', font='微軟正黑體')
-        self.label.place(x=185, y=y)
+        self.label.place(x=x, y=y)
 
 
-one = CreateVar(274)
-two = CreateVar(355)
-three = CreateVar(436)
-sequence = [one, two, three]
+one = CreateVar(285,270)
+two = CreateVar(285,315)
+three = CreateVar(285,360)
+four = CreateVar(285,405)
+five = CreateVar(285,450)
+sequence = [one, two, three, four, five]
 
 
 # 得到權重
@@ -76,7 +78,7 @@ def restaurants(weight):
             score += AllScore[i][j] * weight[j]
         AllWeightedScore[i.strip('11-20')] = score
     result = list()
-    for i in range(3):
+    for i in range(5):
         for j in AllWeightedScore.keys():
             if AllWeightedScore[j] == max(AllWeightedScore.values()):
                 result.append(j)
@@ -88,12 +90,12 @@ def restaurants(weight):
 # 按鈕啟動函數
 def click():
     global sequence
-    for i in range(3):
+    for i in range(5):
         sequence[i].var.set(restaurants(weight())[i])
 
 # 按鈕
 search_btn = Button(window, text='開始搜尋', command=lambda: click(),
                     bg='white', font='微軟正黑體')
-search_btn.place(x=870, y=66)
+search_btn.place(x=970, y=66)
 
 window.mainloop()
